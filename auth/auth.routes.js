7f699bc758305abdff63b77032106607d2ea18b6
Alexander Lopez <alopez@sma.com.mx>
2022-07-13 20:09:04 -0500

    CLEAN CODEX

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -3,6 +3,4 @@ const Users = require('./auth.controller');
 module.exports = (router) => {
     router.post('/register', Users.createUser);
     router.post('/login', Users.loginUser);
-
-    console.log("test backend 3");
 }
