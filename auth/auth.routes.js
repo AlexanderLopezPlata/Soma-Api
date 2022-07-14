1abe33de7a1c3ab95f653304a3f0f3d9f8f331d8
Alexander Lopez <alopez@sma.com.mx>
2022-07-13 19:31:19 -0500

    First commit c:

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -3,4 +3,6 @@ const Users = require('./auth.controller');
 module.exports = (router) => {
     router.post('/register', Users.createUser);
     router.post('/login', Users.loginUser);
+
+    console.log("test");
 }
